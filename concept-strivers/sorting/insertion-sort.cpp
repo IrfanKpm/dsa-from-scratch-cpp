33c34011a2c703d837f1c3d9eb08d33b7a30dabd
IrfanKpm <muhammedirfanp002@gmail.com>
2026-06-20 18:47:18 +0530

    sorting: implement insertion sort algorithm

---
+++ concept-strivers/sorting/insertion-sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {13, 46, 25, 63, 18, 34};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
