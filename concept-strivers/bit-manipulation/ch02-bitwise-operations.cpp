360d1e84d7c9391bae805d71b2ea7c155708d5a2
IrfanKpm <muhammedirfanp002@gmail.com>
2026-07-01 19:33:20 +0530

    bit-manipulation : practiced bitwise OR,XOR using bitset

--- concept-strivers/bit-manipulation/ch02-bitwise-operations.cpp
+++ concept-strivers/bit-manipulation/ch02-bitwise-operations.cpp
@@ -19,10 +19,23 @@ int main(){
     cout << "b2 = x2-bit --> " << b2 << endl;
     cout << "b3 = x3-bit --> " << b3 << endl;
 
-    cout << "\nAND OPERATION" << endl;
+    cout << "\nAND(&) OPERATION" << endl;
     cout << "b2-bit  --> " << b2 << endl;
     cout << "b3-bit  --> " << b3 << endl;
     cout << "b2 & b3 --> " << (b2 & b3) << endl;
+
+    cout << "\nOR(|) OPERATION" << endl;
+    cout << "b2-bit  --> " << b2 << endl;
+    cout << "b3-bit  --> " << b3 << endl;
+    cout << "b2 | b3 --> " << (b2 | b3) << endl;
+
+    cout << "\nXOR(^) OPERATION" << endl;
+    cout << "3 in-bit   --> " << bitset<8>(3) << endl;
+    cout << "0 in-bit   --> " << bitset<8>(0) << endl;
+    cout << "3 ^ 0      --> " << bitset<8>(3 ^ 0) << endl; 
+    cout << "3 ^ 1      --> " << bitset<8>(3 ^ 1) << endl; 
+    cout << "3 ^ 3      --> " << bitset<8>(3 ^ 3) << endl; 
+
 
     return 0;
 }
